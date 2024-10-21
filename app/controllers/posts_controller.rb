bf0b3a25864d11d2d51e63b1b69fe8340b480586
ゆうせい <Ikeura0604@gmail.com>
2024-10-21 22:29:57 +0900

    投稿の新規作成機能の導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,60 +2,36 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   skip_before_action :require_login, only: %i[index]
 
-  # GET /posts or /posts.json
+
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = t("flash.created", item: Post.model_name.human)
+      redirect_to root_path
+    else
+      flash.now[:alert] = t("flash.not_created", item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
-    @post.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -66,6 +42,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:title, :body)
     end
 end
