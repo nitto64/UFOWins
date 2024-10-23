37d8e0eb4e4752c514ff38e8315b4ee67e6d42d5
ゆうせい <Ikeura0604@gmail.com>
2024-10-23 21:56:19 +0900

    メイン画像のアップロードと表示の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
   # アソシエーション
   belongs_to :user
-  has_one_attached :thumbnail
+  has_one_attached :thumbnail, dependent: :destroy
+  has_many_attached :main_images, dependent: :destroy
 
   # バリデーション
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  validates :thumbnail, attachment: { content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 15.megabytes, purge: true }
+  validates :main_images, attachment: { content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 15.megabytes, purge: true }
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    # 複数画像の空の値が送信されないように
+    config.active_storage.multiple_file_field_include_hidden = false
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
